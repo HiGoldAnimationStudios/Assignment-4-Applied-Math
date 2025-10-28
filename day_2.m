d4449239b71c89443ecf6d4d7ad53e01917f6afa
lucienjll <llin@olin.edu>
2025-10-28 16:27:54 -0400

    wrote a bit of day 2

--- day_2.m
+++ day_2.m
@@ -1,8 +1,4 @@
-%unused
-
-%alpha is the maximum multiplier for step size increase
-
-function main_runner()
+function day_2()
 
     DormandPrince = struct();
     DormandPrince.C = [0, 1/5, 3/10, 4/5, 8/9, 1, 1];
@@ -16,7 +12,7 @@ function main_runner()
     9017/3168, -355/33, 46732/5247, 49/176, -5103/18656, 0,0;...
     35/384, 0, 500/1113, 125/192, -2187/6784, 11/84,0];
     
-    t = 0;
+    t = 5;
     XA = 1;
     h = 0.5;
     my_rate = @(t, XA) rate_func01(t,XA);
@@ -24,11 +20,32 @@ function main_runner()
 
     [XB1, XB2, num_evals] = RK_step_embedded(my_rate,t,XA,h,DormandPrince);
 
-    disp(XB1)
-    disp(XB2)
-    disp(num_evals)
+    % disp(['XB1 = ', num2str(XB1)])
+    % disp(['XB2 = ', num2str(XB2)])
+    % disp(['num_evals = ', num2str(num_evals)])
 
     X = solution01(t+h);
-    disp(X)
-   
+    %disp(['X(',num2str(t+h),') = ',num2str(X)])
+    
+    XB_diff = norm(XB1 - XB2);
+
+    %disp(XB_diff)
+ 
+    %plotting XB_diff vs h_ref_list at one specific time constant
+    n_samples=160;
+    h_ref_list=logspace(-6,1,n_samples);
+    XB_diff_list = zeros(1,n_samples);
+
+    for i = length(h_ref_list)
+        
+        h = h_ref_list(i); 
+        [XB1, XB2, ~] = RK_step_embedded(my_rate,t,XA,h,DormandPrince);
+        XB_diff = norm(XB1 - XB2);
+        XB_diff_list(i) = XB_diff;
+        
+    end 
+    
+    disp(XB_diff_list)
+    figure; 
+    plot(h_ref_list, XB_diff_list)
 end 
